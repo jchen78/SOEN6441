ab2d37a8a8a69fd9c2c58766c04bae5e898e32b7
Jason Chen <jasonbiochem@gmail.com>
2015-01-28 20:02:10 -0500

    Refactor the test
    Same as before, but unnecessary files now no longer included in the
    commit

--- GameEngine/src/tests/EngineTests.java
+++ GameEngine/src/tests/EngineTests.java
@@ -3,25 +3,35 @@ package tests;
 import static org.junit.Assert.*;
 import game.engine.Bank;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class EngineTests {
 	private final static int BANK_INITIAL_AMOUNT = 120;
 	
+	private Bank _testBank;
+	
+	@Before
+	public void Initialize()
+	{
+		_testBank = new Bank(BANK_INITIAL_AMOUNT);
+	}
+	
 	@Test
 	public void GivenNoTransactionsWhenQueryingBankShouldReturnInitialValue() {
-		Bank newBank = new Bank(BANK_INITIAL_AMOUNT);
-		assertEquals(BANK_INITIAL_AMOUNT, newBank.getBalance());
+		assertEquals(BANK_INITIAL_AMOUNT, _testBank.getBalance());
 	}
 	
 	@Test
 	public void GivenDebitTransactionWhenQueryingBankShouldReturnCorrectAmount() {
+		// Arrange
 		int someAmount = 6;
 		int newBalance = BANK_INITIAL_AMOUNT - someAmount;
-		Bank newBank = new Bank(BANK_INITIAL_AMOUNT);
 		
-		newBank.withdraw(someAmount);
+		// Act
+		_testBank.withdraw(someAmount);
 		
-		assertEquals(newBalance, newBank.getBalance());
+		// Assert
+		assertEquals(newBalance, _testBank.getBalance());
 	}
 }

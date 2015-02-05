203c5768b641b9f78f8333574052bcad06c97afd
Jason Chen <jasonbiochem@gmail.com>
2015-02-04 19:51:47 -0500

    Player class, with inline documentation.

--- GameEngine/src/game/engine/Player.java
+++ GameEngine/src/game/engine/Player.java
@@ -1,259 +1,199 @@
 package game.engine;
 
-import game.error.BankException;
-import game.error.EntityNotSetException;
-import game.error.InvalidEntityNameException;
+import game.error.InvalidOperationException;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Player {
-	
-	public String playerName;
-	public String playerColor;
-	public int numofPlayerBuildings;
-	int buildingCost;
-	public int numofPlayerMinions;
-	public String playerPersonality;
-	public int playerMoney;
-	public int playerId;
-	public ArrayList<String> playerCards;
-	private static Bank bankAccsess = new Bank ();
-	//Map mapA= new Map();
-	PlayerCard cardSet = new PlayerCard();
-	static Player[] players= new Player[4];
+	private String playerName;
+	private String playerColor;
+	private int numOfPlayerBuildings;
+	private int numOfPlayerMinions;
+	private String playerPersonality;
+	private int playerMoney;
+	private ArrayList<String> playerCards;
 
-	
-	
-   public Player(String name, String color, int minions, int buildings, String personality, int money, int Id, ArrayList<String> initialCards) {
-		this.playerName = name;
-		this.playerColor = color;
-		this.numofPlayerMinions = minions;
-		this.numofPlayerBuildings = buildings;
-		this.playerPersonality = personality;
-		this.playerMoney = money;
-		this.playerId=Id;
-		this.playerCards= initialCards;
-		
-		}
-   
-   public static void main(String [] args){
-	   int i;
-		//players[0]=new Player("...." );
-		//players[1]=new Player( ".....");
-		//players[2]=new Player( ".....");
-		//players[3]=new Player("......");
-		
-	   // print the list of players
-	   System.out.println(" The players of the game are:");
-		for (i=0; i<4; i++){
-	     System.out.println("Player number "+ i + ": " + players[i].getName()); }
-   }
-   
-   // Minion
-   void addMinion( int minionCount,int areaIndex){
-		if (minionCount <= numofPlayerMinions)
-				{
-			//mapA.area[areaIndex].addMinion(playerId, minionCount);	
-			 numofPlayerMinions-= minionCount;
-				}
-		else
-		{
-			System.out.println("No enough available minions!!!");
-		}
-   }
-	 
-	   void removeMinion(int minionCount, int areaIndex){
-		   //mapA.area[areaIndex].removeMinion(playerId, minionCount);
-		  numofPlayerMinions+= minionCount;
-     }
-   
-   // Trouble
-   void addTroubleMarker(int areaIndex){
-//	 
-//	   if (mapA.area[areaIndex].isTroubleMarker() == true){
-//		   System.out.println("Unable to add TroubleMarker, there is already a troubleMarker in area []: "+ areaIndex);
-//	   }
-//	   else
-//	   {
-//		   mapA.area[areaIndex].setTroubleMarker(true);
-//	   }
-   }
-		  
-   void removeTroubleMarker(int areaIndex){
-//	   mapA.area[areaIndex].setTroubleMarker(false);
-	   }
-   
-   // Building
-   void removeBuilding(int areaIndex){
-	 
-//	   mapA.area[areaIndex].removeBuilding(playerId);
-	   numofPlayerBuildings+= 1;
-   }
-      
-   void addBuilding(int areaIndex){
-	   int NoBuilding;
-	   int buildingCost = 0;
-	   boolean withdrawResult=false;
-//	   buildingCost= mapA.area[areaIndex].getBuildingCost();
-//	   NoBuilding=mapA.area[areaIndex].getNoOfBuildings();
-	   
-	   if (buildingCost < playerMoney){
-		   
-	       System.out.println("There is Enough Money to add building");  
-//		   if (!((NoBuilding==1) || (mapA.area[areaIndex].isTroubleMarker()==true)))
-//		   {
-//			    mapA.area[areaIndex].addBuilding(playerId);
-//				playerMoney-=mapA.area[areaIndex].getBuildingCost();
-//				System.out.println("Bulding added to Area ["+ areaIndex+ " ]"); 
-//		   }
-//		   else{
-//			    System.out.println("Already a bulding or a trouble marker exists in this area , new bulding can not be added");
-//		   }
-	   }  
-	   else { 
-		    withdrawResult= takeMoney(buildingCost);
-		     if (withdrawResult=true){
-		    	System.out.println("Money is withdrawn successfully for adding building");
-//			          if (!((NoBuilding==1) || (mapA.area[areaIndex].isTroubleMarker()==true))){
-//			               mapA.area[areaIndex].addBuilding(playerId);
-//					       playerMoney-=mapA.area[areaIndex].getBuildingCost();
-//					       System.out.println("Bulding added to Area ["+ areaIndex+ " ]");  
-//			          }else {
-//			        	  System.out.println("Already a bulding or a trouble marker exists in this area , new bulding can not be added");
-//			          }
-			} 
-		    else {
-					System.out.println("Money could'nt get withdrawn");
-			}
-			   
-		}
-		    	   
-	
+	/**
+	 * Gets the user-friendly player name.
+	 */
+    public String getName() {
+		return playerName;
 	}
-	       
-   // Money
-   public boolean takeMoney(int buildingCost){
-	   boolean withdrawResult=false;
-//	   try {
-//		withdrawResult =bankAccsess.withdraw(buildingCost);
-//	} catch (BankException e) {
-//		e.printStackTrace();
-//	}
-	   return withdrawResult;
-   }
+	
+    /**
+     * Sets the user-friendly player name, intended for display purposes.
+     * @param name Player's name
+     * @throws InvalidOperationException Thrown when the name contains a semicolon character.
+     */
+    public void setName(String name) throws InvalidOperationException {
+    	if (name.contains(";"))
+    		throw new InvalidOperationException("Player name cannot contain the semicolon character.");
+    	
+		this.playerName = name;
+	}
+	
+    /**
+     * Gets the player color, as set by the game manager.
+     * @return The game manager-supplied player color.
+     */
+	public String getplayercolor() {
+		return playerColor;
+	}
+	
+	/**
+	 * Allows the game manager to initialize the player's color.
+	 * @param playercolor Name of the color, as can later be recognized by the game manager.
+	 * @throws InvalidOperationException Thrown when the color contains a semicolon character.
+	 */
+	public void setplayercolor(String playercolor) throws InvalidOperationException {
+		if (playerColor.contains(";"))
+			throw new InvalidOperationException("The player color cannot contain the semicolon character.");
+		
+		this.playerColor = playercolor;
+	}
+	
+	/**
+	 * Gets the number of minions currently in the player's possession, i.e. not on the board.
+	 */
+	public int getNumberOfMinionsInHand() {
+		return numOfPlayerMinions;
+	}
+	
+	/**
+	 * Initializes the number of minions in the player's possession, i.e. not on the board.
+	 * @param minionCount Number of minions.
+	 * @throws InvalidOperationException Thrown when the number of minions is not in [0, 12]
+	 */
+	public void setMinion(int minionCount) throws InvalidOperationException {
+		if (minionCount < 0 || minionCount > 12)
+			throw new InvalidOperationException("Number of minions must be valid.");
+		
+		numOfPlayerMinions = minionCount;
+	}
+	
+	/**
+	 * Gets the number of buildings in the player's possession, i.e. not on the board.
+	 */
+	public int getNumberOfBuildingsInHand() {
+		return numOfPlayerBuildings;
+	}
    
-        public int getPlayerId() {
-		  return playerId;
-	    }
-
-	    public void setPlayerId(int id) {
-		   this.playerId = id;
-	    }
-	    
-    	public String getName() {
-			return playerName;
-		}
-
-		public void setName(String name) {
-			this.playerName = name;
-		}
+	/**
+	 * Initializes the number of buildings in the player's hand.
+	 * @param buildingCount Number of buildings in the player' hand after initialization.
+	 * @throws InvalidOperationException Thrown when the number of buildings is not in [0, 6]
+	 */
+	public void setNumberBuildings(int buildingCount) throws InvalidOperationException {
+		if (buildingCount < 0 || buildingCount > 6)
+			throw new InvalidOperationException("Number of buildings must be valid.");
+	   
+		this.numOfPlayerBuildings = buildingCount;
+	}
+	
+	/**
+	 * Gets the name of the player's personality card as supplied by the game manager.
+	 */
+	public String getPersonality() {
+		return playerPersonality;
+	}
+	
+	/**
+	 * Initializes the personality card in the player's possession.
+	 * @param personality Name of the card as recognizable by the game manager.
+	 * @throws InvalidOperationException Thrown when the name contains a semicolon.
+	 */
+	public void setPersonality(String personality) throws InvalidOperationException {
+		if (personality.contains(";"))
+			throw new InvalidOperationException("The personality card name cannot contain the semicolon character.");
 		
-		public String getplayercolor() {
-			return playerColor;
-		}
+		this.playerPersonality = personality;
+	}
+	
+	/**
+	 * Gets the money currently in the player's hand.
+	 */
+	public int getPlayerMoney() {
+		return playerMoney;
+	}
+	
+	/**
+	 * Initializes the money in the player's hand.
+	 * @param money Amount in the player's hand after initialization.
+	 * @throws InvalidOperationException Thrown when money is not in [0, 120].
+	 */
+	public void setPlayerMoney(int money) throws InvalidOperationException {
+		if (money < 0 || money > 120)
+			throw new InvalidOperationException("Amount of money must be valid.");
+	   
+		playerMoney = money;
+	}
+	
+	/**
+	 * Gets the list of player cards in the current player's hand.
+	 * @return List of card names, as recognizable by the game manager.
+	 */
+	public ArrayList<String> getPlayerCards() {
+		return playerCards;
+	}
+	
+	/**
+	 * Initializes the set of player cards in the current player's hand.
+	 * @param cardNames List of card names, as recognizable by the game manager.
+	 * @throws InvalidOperationException Thrown when at least one card name contains a semicolon character.
+	 */
+	public void setPlayerCards(ArrayList<String> cardNames) throws InvalidOperationException {
+		for (String name : cardNames)
+			if (name.contains(";"))
+				throw new InvalidOperationException("Card names cannot contain the semicolon character.");
 		
-		public void setplayercolor(String playercolor) {
-			this.playerColor = playercolor;
-		}
-		public int getnumofminions() {
-			return numofPlayerMinions;
-		}
-
-		public void setnumofminions(int numofminions) {
-			this.numofPlayerMinions = numofminions;
-		}
-
-		public int getnumofBuildings() {
-			return numofPlayerBuildings;
-		}
-
-		public void setnumofbuildings(int numofbuildings) {
-			this.numofPlayerBuildings = numofbuildings;
-		}
+		this.playerCards = cardNames;
+	}
+	
+	/**
+	 * Serializes the current player's state.
+	 */
+	public String getCurrentState() {
+		String currentState =
+			valueOrEmpty(playerName) + ";"
+			+ valueOrEmpty(playerColor) + ";"
+			+ numOfPlayerBuildings + ";"
+			+ numOfPlayerMinions + ";"
+			+ valueOrEmpty(playerPersonality) + ";"
+			+ playerMoney;
 		
-		public String getPersonality() {
-			return playerPersonality;
-		}
-
-		public void setpersonality(String personality) {
-			this.playerPersonality = personality;
-		}
-		public int getPlayerMoney() {
-			return playerMoney;
-		}
-
-		public void setPlayerMoney(int amountofmoney) {
-			this.playerMoney = amountofmoney;
-		}
+		for (String name : playerCards)
+			currentState += ";" + valueOrEmpty(name);
 		
-		public ArrayList<String> getPlayerCards() {
-			return playerCards;
-		}
-
-		public void setPlayerCards(ArrayList<String> cardNames) {
-			this.playerCards = cardNames;
-		}
+		return currentState;
+	}
+	
+	private String valueOrEmpty(String value) {
+		return value == null ? "" : value;
+	}
+	
+	/**
+	 * Loads the player's state from serialized data.
+	 * @see getCurrentState
+	 * @param serializedData Current state, as retrieved from the <code>getCurrentState</code> method.
+	 * @throws NumberFormatException Thrown when the serialized data is invalid.
+	 * @throws InvalidOperationException Thrown when the serialized data is invalid.
+	 */
+	public void loadCurrentState(String serializedData) throws NumberFormatException, InvalidOperationException {
+		if (serializedData == null)
+			throw new InvalidOperationException("Serialized data cannot be null.");
 		
+		String[] serializedParts = serializedData.split(";");
+		if (serializedParts.length < 6 || serializedParts.length > 106)
+			throw new InvalidOperationException("Invalid serialized data.");
 		
-		// prints the name, color, personality and the cards of all players 		
-		void GetCurrentState(){
-			int i,j;
-			
-			String cardColor="NO COLOR";
-			int cardIndex=-1;
-			String nameP;
-			String colorP, personalityP;
-			
-			// assuming there are 4 players (j is number of total players)
-			for (j=0; j<4; j++)
-			{
-				System.out.println("Player " +j +" : ");
-				System.out.println("---------------------------------------");
-				nameP=players[j].getName();
-				System.out.println("Player name is: "+ nameP );
-				colorP=players[j].getplayercolor();
-				System.out.println("Player color is:  "  + colorP);
-				personalityP=players[j].getPersonality();
-				System.out.println("Player personality is:  "  + personalityP);
-				
-				// is the number of cards the player has
-			    for (i=0; i<playerCards.size(); i++){
-				  try {
-					players[j].cardSet.setEntity(playerCards.get(i));
-					System.out.println("Player cards are:");
-					try {
-						cardColor=cardSet.getBorderColor();
-						System.out.print("Card ["+ i+ " ] : " + cardColor +",  ");
-					} catch (EntityNotSetException e) {
-						e.printStackTrace();
-					}
-//					 try {
-//						cardIndex=cardSet.getCardIndex();
-//						System.out.println(cardIndex);
-//					} catch (EntityNotSetException e) {
-//						
-//						e.printStackTrace();
-//					}
-				} catch (InvalidEntityNameException e) {
-						e.printStackTrace();
-				}
-					    
-		     }// END FOR 2
-	}//end for 1
-     
-
-		
-}
-				
+		playerName = serializedParts[0];
+		playerColor = serializedParts[1];
+		numOfPlayerBuildings = Integer.parseInt(serializedParts[2]);
+		numOfPlayerMinions = Integer.parseInt(serializedParts[3]);
+		playerPersonality = serializedParts[4];
+		playerMoney = Integer.parseInt(serializedParts[5]);
+		playerCards = new ArrayList<String>();
+		for (int i = 6; i < serializedParts.length; i++)
+			playerCards.add(serializedParts[i]);
+	}
 }

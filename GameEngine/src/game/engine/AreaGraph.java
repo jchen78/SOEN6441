38a25145df1c035c3eaba2453034668da5f5d1a5
Deepak <Deepak@rajni.wireless.concordia.ca>
2015-03-10 13:47:58 -0400

    Changes in Map.java class

--- GameEngine/src/game/engine/AreaGraph.java
+++ GameEngine/src/game/engine/AreaGraph.java
@@ -1,21 +1,34 @@
 package game.engine;
 
+
+/*
+    This Class tries to makes a graph of the city areas. We have 12 areas, so 
+    here we have 12 nodes. Edges in this graph show the neighbourhood of two areas.
+    If there is an edge between two nodes of the graph, that means the corresponding areas have 
+    common border or a bridge between them.
+*/
 public class AreaGraph
 {
+    // as the Adjacency Matrix of the Graph
 	private boolean adjacencyMatrix [][];
+    
+    // number of nodes in the Graph, in our example we have 12 nodes (areas)
 	private int vertexCount;
 
-	/**
-	 * Creates an {@link AreaGraph}
-	 * 
-	 * @param vertexCnt vertex count
-	 */
+    /* 
+        Constructor of the Graph
+        @input: number of nodes of the Graph
+        We initialize the Adjacency Matrix
+    */
 	public AreaGraph(int vertexCnt)
 	{
 		this.vertexCount = vertexCnt+1;
 		adjacencyMatrix = new boolean[vertexCnt+1][vertexCnt+1];
 	}
 	
+    /*
+        Adds an edge between node i and node j
+    */ 
 	public void addEdge(int i, int j)
 	{
 		if(i > 0 && i <= vertexCount && j > 0 && j <= vertexCount)
@@ -25,6 +38,9 @@ public class AreaGraph
 		}
 	}
 
+    /*
+        Removes the edge between node i and node j
+    */ 
 	public void removeEdge(int i, int j)
 	{
 		if(i >= 0 && i <= vertexCount && j > 0 && j <= vertexCount)
@@ -35,6 +51,9 @@ public class AreaGraph
 
 	}
 	
+    /*
+        Tests whether there is a graph between node i and node j
+    */ 
 	public boolean isEdge(int i, int j)
 	{
 		if(i > 0 && i <= vertexCount && j > 0 && j <= vertexCount)

342f1cdb4e22fb622acb74f445b3bcfb316bc2d6
Deepak Aggarwal <arn.deepak1@gmail.com>
2015-03-13 11:32:30 -0400

    Update IActionWithEntityRemoval.java

--- GameEngine/src/game/action/scaffold/dataGathering/IActionWithEntityRemoval.java
+++ GameEngine/src/game/action/scaffold/dataGathering/IActionWithEntityRemoval.java
@@ -1,5 +1,11 @@
 package game.action.scaffold.dataGathering;
 
+/**
+ * 
+ * IActionWithEntityRemoval.java
+ * 
+ * /
+
 public interface IActionWithEntityRemoval {
 	void setAreaForRemoval(String validSelectedAreaName) throws Exception;
 }

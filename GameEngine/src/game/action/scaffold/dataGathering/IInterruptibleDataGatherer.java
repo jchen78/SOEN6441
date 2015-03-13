ab494eb901b3131a4f73797106ba3a326c14e68a
Deepak Aggarwal <arn.deepak1@gmail.com>
2015-03-13 11:33:30 -0400

    Update IInterruptibleDataGatherer.java

--- GameEngine/src/game/action/scaffold/dataGathering/IInterruptibleDataGatherer.java
+++ GameEngine/src/game/action/scaffold/dataGathering/IInterruptibleDataGatherer.java
@@ -3,6 +3,13 @@ package game.action.scaffold.dataGathering;
 import game.action.scaffold.IActionVisitee;
 import game.action.scaffold.IExecutableAction;
 
+/**
+ * 
+ * 
+ * IInterruptibleDataGathering.java
+ * 
+ * /
+
 public interface IInterruptibleDataGatherer extends IActionDataGatherer, IExecutableAction, IActionVisitee {
 	String getInterruptorName();
 }

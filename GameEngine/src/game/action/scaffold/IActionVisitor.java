c3e6abe5f101618554399ae51eb0300989c1ec0b
Deepak Aggarwal <arn.deepak1@gmail.com>
2015-03-13 11:37:22 -0400

    Update IActionVisitor.java

--- GameEngine/src/game/action/scaffold/IActionVisitor.java
+++ GameEngine/src/game/action/scaffold/IActionVisitor.java
@@ -2,6 +2,13 @@ package game.action.scaffold;
 
 import game.error.ActionException;
 
+
+/**
+ * 
+ * IActionVisitor.java
+ * 
+ * /
+ 
 public interface IActionVisitor {
 	void visit(IActionVisitee visitee) throws ActionException;
 }

266470e8deb2edd7e846c6b1845d6a8bb2d2d0cb
Deepak Aggarwal <arn.deepak1@gmail.com>
2015-03-13 11:38:15 -0400

    Update IConditionalAction.java

--- GameEngine/src/game/action/scaffold/IConditionalAction.java
+++ GameEngine/src/game/action/scaffold/IConditionalAction.java
@@ -2,6 +2,14 @@ package game.action.scaffold;
 
 import game.engine.GameManager;
 
+
+/**
+ * 
+ * 
+ * IConditionalAction.java
+ * 
+ * /
+
 public interface IConditionalAction extends IOptionalAction {
 	boolean isActionApplicable(GameManager gameInstance, String currentPlayerName) throws Exception;
 }

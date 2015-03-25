56fdd0c5fe0e17216e9f83577032c31f374ee86a
Deepak Aggarwal <arn.deepak1@gmail.com>
2015-03-24 21:29:27 -0400

    Details added to RandomEventCardName

--- GameEngine/src/game/core/enums/RandomEventCardName.java
+++ GameEngine/src/game/core/enums/RandomEventCardName.java
@@ -1,7 +1,18 @@
 package game.core.enums;
 
 public enum RandomEventCardName implements ICardName {
-	Fog("Fog");
+	Fog("Fog"),
+	Fire("Fire"),
+	MysteriousMurder("MysteriousMurder"),
+	Riots("Riots"),
+	Subsidence("Subsidence"),
+	Troll("Troll"),
+	BloodyStupidJhonson("BloodyStupidJhonson"),
+	DemonsFromDungeonDimensions("DemonsFromDungeonDimensions"),
+	TheDragon("TheDragon"),
+	Earthquake("Earthquake"),
+	Explosion("Explosion"),
+	Flood("Flood");
 	
 	private String _cardName;
 	
@@ -17,8 +28,32 @@ public enum RandomEventCardName implements ICardName {
 	@Override
 	public String getDescriptiveName() {
 		switch (_cardName) {
+		case "Fog":
+			return "Fire";
+		case "Fire":
+			return "Fire";
+		case "MysteriousMurder":
+			return "MysteriousMurder";
+		case "Riots":
+			return "Riots";
+		case "Subsidence":
+			return "Subsidence";
+		case "Troll":
+			return "Troll";
 			default:
 				return _cardName;
+			case "BloodyStupidJhonson":
+				return "Bloody Stupid Jhonson";
+			case "DemonsFromDungeonDimensions":
+				return "Demons From Dungeon Dimensions";
+			case "TheDragon":
+				return "The Dragon";
+			case "Earthquake":
+				return "Earthquake";
+			case "Explosion":
+				return "Explosion";
+			case "Flood":
+					return "Flood";
 		}
 	}
 }

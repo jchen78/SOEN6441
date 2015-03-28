dd94410b1f09e5cf17651c0c5be78db9009584be
Deepak Aggarwal <arn.deepak1@gmail.com>
2015-03-28 15:16:24 -0400

    Changes in PlayerCard

--- GameEngine/src/game/action/sequence/visitee/PlaceBuilding.java
+++ GameEngine/src/game/action/sequence/visitee/PlaceBuilding.java
@@ -2,18 +2,13 @@ package game.action.sequence.visitee;
 
 import game.action.sequence.interfaces.IVisitee;
 import game.action.sequence.interfaces.IVisitor;
-import game.core.interfaces.IPlayer;
-import game.engine.*;
 
-public class EvaluateEscapeActionVisitee implements IVisitee {
-	public EvaluateEscapeActionVisitee(IPlayer targetPlayer, ActionType typeOfAction, ActionName actionToInterrupt) {
-		
-	}
-	
+public class PlaceBuilding implements IVisitee {
+
 	@Override
 	public void accept(IVisitor visitor) throws GameOverException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -21,8 +16,5 @@ public class EvaluateEscapeActionVisitee implements IVisitee {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	public boolean isActionInterrupted() {
-		return false;
-	}
+
 }

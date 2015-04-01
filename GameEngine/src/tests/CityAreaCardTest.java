f94edda11241e2453973766ad39d30046a70cbd6
Deepak Aggarwal <arn.deepak1@gmail.com>
2015-04-01 15:49:23 -0400

    Changes to CityAreaCardTest

--- GameEngine/src/tests/CityAreaCardTest.java
+++ GameEngine/src/tests/CityAreaCardTest.java
@@ -21,13 +21,39 @@ public class CityAreaCardTest {
 	@Test
 	public void testGetCardname() throws InvalidEntityNameException {
 		String valueBefore = cac.getCardname();
-		System.out.println(valueBefore);
+	//	System.out.println(valueBefore);
 		String st= "Dimwell";
 		cac.setEntity(st);
 		assertEquals("Dimwell", cac.getCardname());
 		String valueAfter = cac.getCardname();
-		System.out.println(valueAfter);
+	//	System.out.println(valueAfter);
 	}
+
+
+
+	@Test
+	public void testGetCardnameForDifferentValue() throws InvalidEntityNameException {
+	String valueBefore = cac.getCardname();
+//	System.out.println(valueBefore);
+	String st= "UnrealEstate";
+	cac.setEntity(st);
+	assertNotEquals("NapHill", cac.getCardname());
+	String valueAfter = cac.getCardname();
+//	System.out.println(valueAfter);
+}
+	
+	@Test
+	public void testGetCardnameException() {
+	String valueBefore = cac.getCardname();
+	String st= "Unrealstate";
+	try {
+		cac.setEntity(st);
+		assertNotEquals("NapHill", cac.getCardname());
+	} catch (InvalidEntityNameException e) {
+		System.out.println("Invalid AreaCard String");
+	}
+	
+}
 }
 
 	

fab028ff934ae56f9f4b7369e5c6e8b82f67aca1
faribataheri <faribataheri1@gmail.com>
2015-04-02 17:11:03 -0400

    Update TakeLoanFromBankVisitee.java

--- GameEngine/src/game/action/sequence/visitee/TakeLoanFromBankVisitee.java
+++ GameEngine/src/game/action/sequence/visitee/TakeLoanFromBankVisitee.java
@@ -13,8 +13,8 @@ public class TakeLoanFromBankVisitee implements IVisitee {
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "Take loan from the bank";
 	}
 
 }
